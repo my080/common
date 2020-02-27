41a21cf85bb5db031666ef197225456e15b7d7f9
zhouliang <zhouliang@yondervision.com.cn>
2020-02-27 14:13:21 +0800

    增加 storage 类

---
+++ index.js
@@ -0,0 +1,12 @@
+
+import CookiesStorage from 'src/common/scripts/storage/promise/cookies-stroage.js'
+import LocalStorage from 'src/common/scripts/storage/promise/local-storage.js'
+import SessionStorage from 'src/common/scripts/storage/promise/session-storage.js'
+import WeiStorage from 'src/common/scripts/storage/promise/wei-storage.js'
+
+export default {
+	CookiesStorage,
+	LocalStorage,
+	SessionStorage,
+	WeiStorage
+}
