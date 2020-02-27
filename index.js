9dbdeff877b667c4dc430a1a33f9b5c45b3be549
zhouliang <zhouliang@yondervision.com.cn>
2020-02-27 17:14:34 +0800

    调整一下写法

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
-import CookiesStorage from 'src/common/scripts/storage/promise/cookies-stroage.js'
-import LocalStorage from 'src/common/scripts/storage/promise/local-storage.js'
-import SessionStorage from 'src/common/scripts/storage/promise/session-storage.js'
-import WeiStorage from 'src/common/scripts/storage/promise/wei-storage.js'
+import CookiesStorage from './src/common/scripts/storage/promise/cookies-stroage.js'
+import LocalStorage from './src/common/scripts/storage/promise/local-storage.js'
+import SessionStorage from './src/common/scripts/storage/promise/session-storage.js'
+import WeiStorage from './src/common/scripts/storage/promise/wei-storage.js'
 
-export default {
+export {
 	CookiesStorage,
 	LocalStorage,
 	SessionStorage,
